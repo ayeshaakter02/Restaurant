a383bda376605f739b2947f5e5739f594410b859
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-11-24 14:34:25 +0600

    Update Firebase

--- src/Validate/Login.jsx
+++ src/Validate/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
   //form validation
   function FormValidate(e) {
     if (e.Email === "santonaa641@gmail.com" || "nishatnishu2121@gmail.com" || "kimbora644@gmail.com" || "etee.shuvechha@gmail.com") {
-      if (e.Psw === "1234") {
+      if (e.Psw === "123456") {
         //auth change value and page navigate to home screen
         setAuth(true);
         toast.success("Login Successfully!");
